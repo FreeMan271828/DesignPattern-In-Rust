2baa403e1e658a4b29db5976ddc14ccd4ed796ac
freeman <1114509193@qq.com>
2024-08-31 10:13:05 +0800

    hsy

--- src/behavioral_patterns/iterator.rs
+++ src/behavioral_patterns/iterator.rs
@@ -1,68 +1,72 @@
-pub trait Aggregate<T> {
-    fn new() -> Self;
-    fn add(&mut self, data: T);
-    fn del(&mut self, index: usize) -> Option<&mut Self>;
-    fn count(&self) -> usize;
-}
-
-pub trait Iterator<T> {
-    fn reset(&mut self);
-    fn print(&mut self) -> Option<&T>;
+pub trait Iterator{
+    type Item;
+    fn first(&mut self);
     fn next(&mut self);
-    fn has_next(&self) -> bool;
+    fn is_done(&self) ->bool;
+    fn current_item(&self) -> Option<&Self::Item>;
 }
 
-pub struct List<T> {
-    array: Vec<T>,
-    index: usize,
+/// 工厂模式, 是聚合对象的抽象
+pub trait AbstractList{
+    type Item;
+    fn new()->Self;
+}
+/// 具体的聚合对象, List是基于Vec的聚合对象
+pub trait List: AbstractList{
+    fn new()->Self;
+    fn add(&mut self, data:Self::Item);
+    fn count(&self)->usize;
+    fn get(&self,index:usize)->Option<&Self::Item>;
 }
 
-impl<T> Aggregate<T> for List<T> {
-     fn new() -> Self {
-        List {
-            array: Vec::new(),
-            index: 0,
+pub struct ListIterator<T>{
+    _current:usize,
+    pub _list:Vec<T>,
+}
+impl<T> AbstractList for ListIterator<T> {
+    type Item = T;
+    fn new() -> Self {
+        ListIterator{
+            _current:0,
+            _list: Vec::new()
         }
     }
-
-    fn add(&mut self, data: T) {
-        self.array.push(data);
-    }
-
-    fn del(&mut self, index: usize) -> Option<&mut Self> {
-        if index < self.count() {
-            self.array.remove(index);
-            Some(self)
-        } else {
-            None
+}
+impl<T> List for ListIterator<T>{
+    fn new() -> Self {
+        ListIterator{
+            _current: 0,
+            _list: vec![],
         }
     }
-
+    fn add(&mut self, data: Self::Item) {
+        self._list.push(data);
+    }
     fn count(&self) -> usize {
-        self.array.len()
+        self._list.len()
+    }
+    fn get(&self,index: usize) -> Option<&Self::Item> {
+        if index >= self.count(){
+            panic!("Index-Out-Of-Bound")
+        }
+        self._list.get(index)
     }
 }
-
-impl<T> Iterator<T> for List<T> {
-    fn reset(&mut self) {
-        self.index = 0;
+impl<T> Iterator for ListIterator<T>{
+    type Item = (T);
+    fn first(&mut self) {
+        self._current=0;
     }
-    fn print(&mut self) -> Option<&T> {
-        if self.index>=self.count() {
-            None
-        }
-        else {
-            self.array.get(self.index)
-        }
-    }
-
     fn next(&mut self) {
-        if self.has_next() {
-            self.index += 1;
-        }
+        self._current+=1;
     }
-
-    fn has_next(&self) -> bool {
-        self.index < self.count()
+    fn is_done(&self) -> bool {
+        self._current >= self._list.len()
+    }
+    fn current_item(&self) -> Option<&Self::Item> {
+        if self.is_done(){
+            panic!("Iterator-Out-Of-Bound");
+        }
+        self._list.get(self._current)
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,19 @@
-use crate::behavioral_patterns::iterator::{Aggregate, Iterator, List};
+use crate::behavioral_patterns::iterator::{Iterator, List, ListIterator};
 
 mod behavioral_patterns;
 
-fn print<T>(list: &mut List<T>)
-where T:std::fmt::Display{
-    while list.has_next() {
-        println!("data: {}", list.print().unwrap());
+fn main() {
+    let mut v: ListIterator<i32> = behavioral_patterns::iterator::AbstractList::new();
+    v.add(1); v.add(2);
+    v.add(3); v.add(4);
+    print_list(v);
+}
+fn print_list<T: std::fmt::Display>(mut list: ListIterator<T>){
+    let mut index = 0;
+    list.first();
+    while !list.is_done() {
+        println!("Index:{}, Data:{}", index, list.current_item().unwrap());
+        index+=1;
         list.next();
     }
-    list.reset()
-}
-
-fn main() {
-    let mut vec = List::new();
-    vec.add(1);
-    vec.add(2);
-    println!("{}", vec.count());
-    vec.add(3);
-    vec.del(0);
-    print(&mut vec);
 }
